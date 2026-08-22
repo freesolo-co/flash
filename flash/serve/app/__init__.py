a396000793172e7264c76c840360c4930c4420a9
David Shan <78061174+DavidBShan@users.noreply.github.com>
2026-08-22 21:43:16 +0000

    refactor: drop serving facade exports that have no consumer

--- flash/serve/app/__init__.py
+++ flash/serve/app/__init__.py
@@ -5,28 +5,12 @@ from __future__ import annotations
 import importlib
 
 _EXPORTS = {
-    "MANIFEST_SCHEMA": (".manifest", "MANIFEST_SCHEMA"),
-    "MANIFEST_VERSION": (".manifest", "MANIFEST_VERSION"),
     "AdapterExecutionInput": (".manifest", "AdapterExecutionInput"),
     "ArtifactFile": (".manifest", "ArtifactFile"),
     "ExecutionInputs": (".manifest", "ExecutionInputs"),
-    "ManifestAdapter": (".manifest", "ManifestAdapter"),
     "ManifestError": (".manifest", "ManifestError"),
-    "MaterializationError": (".materialize", "MaterializationError"),
-    "PublishedAdapter": (".bootstrap", "PublishedAdapter"),
-    "ServingBootstrap": (".bootstrap", "ServingBootstrap"),
-    "ServingManifest": (".manifest", "ServingManifest"),
-    "adapter_cache_path": (".materialize", "adapter_cache_path"),
     "aggregate_file_digest": (".manifest", "aggregate_file_digest"),
-    "bootstrap_serving": (".bootstrap", "bootstrap_serving"),
     "build_serving_manifest": (".manifest", "build_serving_manifest"),
-    "create_app": (".http", "create_app"),
-    "engine_config_from_manifest": (".bootstrap", "engine_config_from_manifest"),
-    "hydrate_manifest": (".materialize", "hydrate_manifest"),
-    "load_serving_manifest": (".manifest", "load_serving_manifest"),
-    "locked_manifest_cache": (".materialize", "locked_manifest_cache"),
-    "validate_manifest_cache": (".materialize", "validate_manifest_cache"),
-    "validate_materialized_adapter": (".materialize", "validate_materialized_adapter"),
 }
 
 __all__ = sorted(_EXPORTS)
